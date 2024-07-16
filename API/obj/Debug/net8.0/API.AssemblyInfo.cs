086ee1e5de5627cffc01140599e96b00247ff678
Felipe Ferreira Alves <felipealvesferreira05@gmail.com>
2024-07-15 22:59:48 -0300

    Login e Cadastro feitos
    Terminei a seção 5 do curso

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99464a5182672374d8707706d9cbea2577750004")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+144cf11d11848e52dcdf1c4cdd2c4b3df2694aa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

4bf725be5849a94d7518f2b4c63752f9464a3a46
Felipe Ferreira Alves <felipealvesferreira05@gmail.com>
2024-08-21 17:49:11 -0300

    Voltando ao trabalho
    Fim da seção 5

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+144cf11d11848e52dcdf1c4cdd2c4b3df2694aa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d602fe4abe2f40ce0b3bc9164f8e7c1cabf2431c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_service/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -13,6 +14,7 @@ export class RegisterComponent {
   private accountService = inject(AccountService);
   usersFromHomeComponent = input.required<any>();
   @Output()cancelRegister =  new EventEmitter();
+  private toaster = inject(ToastrService);
   model: any = {};
 
   register(){
@@ -21,7 +23,7 @@ export class RegisterComponent {
         console.log(response);
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => this.toaster.error(error.error)
     })
   }
 

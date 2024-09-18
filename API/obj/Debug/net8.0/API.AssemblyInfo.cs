983c9ce435a8c9b53a45d4f811b9ae70c346d809
Felipe Ferreira Alves <felipealvesferreira05@gmail.com>
2024-09-18 18:05:39 -0300

    Seção 10 - Fim
    Seção 10 finalizada

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94ab15f6f827fae612334a6f70b9e5998d22a6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+320e1394e3a273f45775423fb1018e4453a16366")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

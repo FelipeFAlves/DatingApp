d5bdf7b6db658c83e735c705050de1d418dbce8e
Felipe Ferreira Alves <felipealvesferreira05@gmail.com>
2024-10-12 13:22:33 -0300

    Parcial
    Falta só detalhes

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f96e090025e00db970ee87389fbf246ece360b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0516b1663a51f31f8a119dc10a188ca9c008d85")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { LikesService } from '../_service/likes.service';
+import { Member } from '../_models/member';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
+import { MemberCardComponent } from '../members/member-card/member-card.component';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [ButtonsModule,FormsModule,MemberCardComponent],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit{
+  
+  private likesService = inject(LikesService);
+  members: Member[]= [];
+  predicate = 'liked';
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  getTitle(){
+    switch (this.predicate){
+      case 'liked': return 'Membros que você curtiu';
+      case 'likedBy': return 'Membros que curtiram você';
+      default: return 'Mutuos'
+    }
+  }
+
+  loadLikes(){
+    this.likesService.getLikes(this.predicate).subscribe({
+      next: members => this.members = members
+    })
+  }
+
+
+
 
 }
